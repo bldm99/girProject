a2225801108004bf848ad46de11a82ac2413dedd
bldm99 <bladimir.chipana@tecsup.edu.pe>
2024-01-04 03:46:28 +0100

    new test

---
+++ src/pruebas/New.jsx
@@ -0,0 +1,16 @@
+
+import React, { useState } from 'react'
+
+import * as Datatest from "./Datatest"
+import Imputx from '../componentes/imputx/Imputx'
+
+
+export const New = () => {
+
+
+  return (
+    <div>
+        <h1>Login Test</h1>
+    </div>
+  )
+}
