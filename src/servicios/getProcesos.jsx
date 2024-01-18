d71d3f3239f2458cbb16d02ef9cbc9ade15ceae4
bldm99 <bladimir.chipana@tecsup.edu.pe>
2024-01-18 15:44:19 -0500

    solucion error getproces3

--- src/servicios/getProcesos.jsx
+++ src/servicios/getProcesos.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 //const URLdesarrollo = "https://girapi.bladimirchipana.repl.co/";
-const URLdesarrollo = "https://490fb1df-9627-4ae2-9252-7a7ddbeb5470-00-3j4e9qkfg6jac.picard.replit.dev/"
+//const URLdesarrollo = "https://490fb1df-9627-4ae2-9252-7a7ddbeb5470-00-3j4e9qkfg6jac.picard.replit.dev/"
+const URLdesarrollo = "https://girapi-dev-prxd.4.us-1.fl0.io/"
 //const   URLdesarrollo = "http://localhost:3000/"
 
 export const getProcesos = async (idUsuario) => {
